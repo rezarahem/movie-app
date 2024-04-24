a074ef2ef54aacbadca452fc59dcdba4bbae2e5e
rahem <reza.rahem224@gmail.com>
2024-04-24 21:45:44 +0330

    init

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'movie-app.storage.iran.liara.space'
+            }
+        ]
+    }
+};
 
 export default nextConfig;
